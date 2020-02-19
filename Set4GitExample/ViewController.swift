d514902e033b75458756f4dbe4b9847de739c4df
Tom Swope <tjswope@gmail.com>
2020-02-19 07:42:52 -0500

    Initial Commit

---
+++ Set4GitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Set4GitExample
+//
+//  Created by Swope, Thomas on 2/19/20.
+//  Copyright © 2020 Swope, Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
